79ad889899184f24792f76bb20ca4f38c8c824a9
costudying <132265893+costudying@users.noreply.github.com>
2023-10-09 14:01:10 +0900

    20231009 JPA연습 INSERT데이터 추가

--- src/test/java/com/example/practice01/game/jpaTest/GameJpaTests.java
+++ src/test/java/com/example/practice01/game/jpaTest/GameJpaTests.java
@@ -36,6 +36,41 @@ public class GameJpaTests {
                         , "NVIDIA GeForce GTX 460 AMD Radeon HD 6870"
                         , "3 GB"
                         )
+                ),
+                Arguments.of(
+                        "AOS"
+                        , "TWELVE"
+                        , "라이엇 게임즈"
+                        , "FC온라인"
+                        , LocalDate.of(2011,12,4)
+                        ,"라이엇 게임즈"
+                        ,new GameSpec(
+                                "Windows 10 (x64)"
+                                , "Intel: Core i5-3300 / AMD: Ryzen 3 1200 / ARM: 미지원"
+                                , "4 GB"
+                                , "SSD 16GB 이상"
+                                , "NVIDIA GeForce 560[13]\n" +
+                                "AMD HD 6950[AMD]\n" +
+                                "Intel UHD 630 내장 그래픽"
+                                , "2 GB"
+                        )
+                ),
+                Arguments.of(
+                        "MMORPG"
+                        , "ALL"
+                        , "넥슨"
+                        , "메이플스토리"
+                        , LocalDate.of(2003,4,29)
+                        ,"넥슨"
+                        ,new GameSpec(
+                                "Windows 7 (x64)"
+                                , "Pentium Dual Core급 이상"
+                                , "4 GB"
+                                , "20GB 이상"
+                                , "GeForce 9600 GT / Radeon HD 5670 이상\n" +
+                                "(Shader 3.0 이상 지원 그래픽 카드)"
+                                , "2 GB"
+                        )
                 )
         );
     }
